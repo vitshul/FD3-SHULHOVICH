6bd1b9358108b7e4f65d5e2590c522dfdeb32067
Vitaly Shulhovich <vital.mywork@gmail.com>
2019-09-11 23:48:31 +0300

    edit

--- ishop/IShop.js
+++ ishop/IShop.js
@@ -1,35 +1,35 @@
-﻿﻿var IShopBlock = React.createClass({
+﻿var IShopBlock = React.createClass({
 
     displayName: 'IShopBlock',
 
-    getDefaultProps: function() {
+    getDefaultProps: function () {
         return { question: "Что-то пошло не так!" }
-      },
+    },
 
-    render: function() {
-  
-      var storageGoods=[];
+    render: function () {
+
+        var storageGoods = [];
 
 
-      this.props.goods.forEach(element => {
-            
-        storageGoods.push(
-            React.DOM.div(
-                    {key: element.code, className: 'Goods'},
-                    React.DOM.div({className:'Foto'},element.urlFoto),
-                    React.DOM.div({className:'Name'}, element.nameGoods),
-                    React.DOM.div({className:'Cost'},element.cost),
-                    React.DOM.div({className:'Balance'},element.balance),
-                    
+        this.props.goods.forEach(element => {
+
+            storageGoods.push(
+                React.DOM.div(
+                    { key: element.code, className: 'Goods' },
+                    React.DOM.div({ className: 'Foto' }, element.urlFoto),
+                    React.DOM.div({ className: 'Name' }, element.nameGoods),
+                    React.DOM.div({ className: 'Cost' }, 'cost: '+element.cost),
+                    React.DOM.div({ className: 'Balance' }, 'balance: '+element.balance),
+
                 )
             );
 
         });
-      
-      return React.DOM.div( {className:'IShopBlock'}, 
-        React.DOM.h1( {className:'NameShop'}, this.props.nameShop ),
-        React.DOM.div( {className:'GoodsStorage'}, storageGoods ),
-      );
+
+        return React.DOM.div({ className: 'IShopBlock' },
+            React.DOM.h1({ className: 'NameShop' }, this.props.nameShop),
+            React.DOM.div({ className: 'GoodsStorage' }, storageGoods),
+        );
     },
-  
-  });
+
+});
