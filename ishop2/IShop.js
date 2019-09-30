cf707f285ec6d804814f58c6930b8837d12da58e
Vitaly Shulhovich <vital.mywork@gmail.com>
2019-09-30 21:50:11 +0300

    clone ishop

---
+++ ishop2/IShop.js
@@ -0,0 +1,38 @@
+﻿var IShopBlock = React.createClass({
+
+    displayName: 'IShopBlock',
+
+    getDefaultProps: function () {
+        return { question: "Что-то пошло не так!" }
+    },
+
+    render: function () {
+
+        var storageGoods = [];
+
+        var img1;
+
+        this.props.goods.forEach(element => {
+
+            storageGoods.push(
+                React.DOM.div(
+                    { key: element.code, className: 'Goods' },
+
+                    React.DOM.img({src: element.urlFoto, alt: element.nameGoods}),
+                    React.DOM.div({ className: 'Name' }, element.nameGoods),
+                    React.DOM.div({ className: 'Cost' }, 'cost: ' + element.cost),
+                    React.DOM.div({ className: 'Balance' }, 'balance: ' + element.balance),
+
+                )
+            );
+
+        });
+
+        return React.DOM.div({ className: 'IShopBlock' },
+            React.DOM.h1({ className: 'NameShop' }, this.props.nameShop),
+            React.DOM.div({ className: 'GoodsStorage' }, storageGoods),
+        );
+    },
+
+});
+
